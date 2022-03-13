b3003ca869f75aa04c7caadbad013a6e0be0d7bf
luboolu <kjjy98@naver.com>
2022-03-14 00:03:01 +0900

    UserSettingViewController Refactoring

--- TimeDiary/TimeDiary/ViewController/UserSettingViewController.swift
+++ TimeDiary/TimeDiary/ViewController/UserSettingViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class UserSettingViewController: UIViewController {
+final class UserSettingViewController: UIViewController {
     
     static let identifier = "UserSettingViewController"
     
@@ -15,21 +15,14 @@ class UserSettingViewController: UIViewController {
     @IBOutlet weak var settingTableView: UITableView!
     
     //let settingList = [["백업하기", "복구하기"], ["개인정보처리방침", "앱 버전"]]
-    let settingList = [[NSLocalizedString("privacy", comment: "개인정보처리방침"), NSLocalizedString("openSourceLicense", comment: "오픈소스 라이센스") , NSLocalizedString("appVersion", comment: "앱 버전")]]
+    private let settingList = [[NSLocalizedString("privacy", comment: "개인정보처리방침"), NSLocalizedString("openSourceLicense", comment: "오픈소스 라이센스") , NSLocalizedString("appVersion", comment: "앱 버전")]]
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        settingTableView.delegate = self
-        settingTableView.dataSource = self
-        
-        self.navigationController?.navigationBar.topItem?.title = NSLocalizedString("setting", comment: "설정")
-        
-        //tabbar setting
-        tabBarController?.tabBar.selectedItem?.title = NSLocalizedString("setting", comment: "설정")
-
-        tabBarController?.tabBar.tintColor = UIColor(named: "bear")
+        setTableView()
+        setNavigationTabBar()
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
@@ -39,13 +32,26 @@ class UserSettingViewController: UIViewController {
     override var prefersStatusBarHidden: Bool {
         return false
     }
+    
+    private func setNavigationTabBar() {
+        //navigationController setting
+        self.navigationController?.navigationBar.topItem?.title = NSLocalizedString("setting", comment: "설정")
+        
+        //tabbar setting
+        tabBarController?.tabBar.selectedItem?.title = NSLocalizedString("setting", comment: "설정")
+        tabBarController?.tabBar.tintColor = UIColor(named: "bear")
+    }
+    
+    private func setTableView() {
+        settingTableView.delegate = self
+        settingTableView.dataSource = self
+    }
 
 }
 
 extension UserSettingViewController: UITableViewDelegate, UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        
         return settingList.count
     }
     
@@ -58,7 +64,6 @@ extension UserSettingViewController: UITableViewDelegate, UITableViewDataSource 
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingTableViewCell.identifier, for: indexPath) as? SettingTableViewCell else {
             return UITableViewCell()
         }
@@ -67,7 +72,6 @@ extension UserSettingViewController: UITableViewDelegate, UITableViewDataSource 
         cell.settingLabel.font = UIFont().kotra_songeulssi_13
         
         return cell
-        
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -75,42 +79,32 @@ extension UserSettingViewController: UITableViewDelegate, UITableViewDataSource 
         tableView.deselectRow(at: indexPath, animated: true)
         //테이블뷰 선택되면 화면전환
         if indexPath.row == 0 {
-            
             let st = UIStoryboard(name: "Privacy", bundle: nil)
             if let vc = st.instantiateViewController(withIdentifier: PrivacyViewController.identifier) as? PrivacyViewController {
                 
-                
                 vc.modalPresentationStyle = .fullScreen
-                    
                 //navigation bar를 포함하여 다음 뷰 컨트롤러로 화면전환 - push
                 self.navigationController?.pushViewController(vc, animated: true)
             }
             
         } else if indexPath.row == 1 {
-            
             let st = UIStoryboard(name: "OpenSourceLicense", bundle: nil)
             if let vc = st.instantiateViewController(withIdentifier: OpenSourceLicenseViewController.identifier) as? OpenSourceLicenseViewController {
                 
-                
                 vc.modalPresentationStyle = .fullScreen
                     
                 //navigation bar를 포함하여 다음 뷰 컨트롤러로 화면전환 - push
                 self.navigationController?.pushViewController(vc, animated: true)
             }
             
-            
-        } else if indexPath.row == 2 {
+        } else {
             let st = UIStoryboard(name: "UserAppVersion", bundle: nil)
             if let vc = st.instantiateViewController(withIdentifier: UserAppVersionViewController.identifier) as? UserAppVersionViewController {
-                
-                
                 vc.modalPresentationStyle = .fullScreen
                     
                 //navigation bar를 포함하여 다음 뷰 컨트롤러로 화면전환 - push
                 self.navigationController?.pushViewController(vc, animated: true)
             }
-        } else {
-            
         }
     }
     
